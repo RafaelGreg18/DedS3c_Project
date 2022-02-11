46c44a343dd84f7f0e38f0f38915a468e094c7d0
Rafael Gregori <rafaelgregori1337@gmail.com>
2022-02-11 16:02:35 -0300

    11/02

---
+++ index.js
@@ -0,0 +1,11 @@
+// function autoScroll(){
+//     window.scrollBy(0,1);
+//     var scrolldelay = setTimeout(autoScroll, 10);
+//     setTimeout(clearTime, 150, scrolldelay);
+// }
+
+// function clearTime(){
+//     clearTimeout(scrolldelay);
+// }
+
+// autoScroll();
